ed9bfa46fe22490528ab5a63e66f002300ec272a
HongHuang <2585854600@qq.com>
2024-06-12 16:18:32 +0800

    Ink

--- IntersectionK.cpp
+++ IntersectionK.cpp
@@ -8,10 +8,6 @@
 #include "Solver/DetectAmbiguity/DetectAmbiguity.h"
 
 
-using namespace std;
-
-
-
 
 int main(int argc, char* argv[]){
   if (argc != 3){
@@ -45,5 +41,4 @@ int main(int argc, char* argv[]){
   else{
     std::cout << argv[1] << " : NoMatch"  <<  std::endl;
   }
-    
 } 
