ba098e3fbbacc50f58d0349189843a20f1d2055d
HongHuang <2585854600@qq.com>
2024-06-14 16:48:00 +0800

    fix reture error

--- Solver/String/regexp_symbolic_Completment.cpp
+++ Solver/String/regexp_symbolic_Completment.cpp
@@ -18,15 +18,27 @@ namespace solverbin{
     this->e1 = e1;
     this->Prefix_string = prefix_string;
     this->D1 = FULLmatchDFA(e1);
+    Alphabet_completment = ComputeAlphabet(e1.ByteMap);
+  }
+
+  std::set<uint8_t> RegExpSymbolic::CompletmentDFA::ComputeAlphabet(uint8_t bytemap[256]){
+    std::set<uint8_t> alphabet_completment = e1.Alphabet;
+    for (int i = 0; i < 256; i++){
+      if (bytemap[i] == 0){
+        alphabet_completment.insert(i);
+      }
+    }
+    return alphabet_completment;
   }
 
   std::string RegExpSymbolic::CompletmentDFA::ComputeCompletmentDFA(){
+    std::string completment_str = Prefix_string;
     FULLmatchDFA::DFAState* curr_ = D1.DState;
     for (auto it : Prefix_string){
       curr_ = D1.StepOneByte(curr_, it);
       if (curr_ == nullptr)
         return "";
     }
-    
+    return completment_str;
   }
 }
