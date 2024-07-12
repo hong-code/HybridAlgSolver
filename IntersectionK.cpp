4ede4a792961599b02b508375168a9b800b06ab3
HongHuang <2585854600@qq.com>
2024-07-12 13:13:18 +0800

    7-12

--- IntersectionK.cpp
+++ IntersectionK.cpp
@@ -44,7 +44,6 @@ int main(int argc, char* argv[]){
   }
   else
     std::cout << "unsat" << std::endl;
-
   // Test our tool.  
   // if ((InK.Intersect() && 1 == std::stoi(argv[2])) || (!InK.Intersect() && 0 == std::stoi(argv[2]))){
   //   std::cout << argv[1] << " : Match"  <<  std::endl;
