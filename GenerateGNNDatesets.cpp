c95464778ca1afb29dd3721284e80f9259acd587
2585854600@qq.com <2585854600@qq.com>
2025-08-21 08:18:46 +0000

    cmake fix

--- GenerateGNNDatesets.cpp
+++ GenerateGNNDatesets.cpp
@@ -5,21 +5,19 @@
 #include <unistd.h>
 #include <torch/torch.h>
 #include "Solver/solver_kind.h"
-#include "Solver/solver.h"
 #include "Parser/parser.h"
+// #include "Solver/PositionAutomaton/PositionAutomaton.h" 
 
-#include "Solver/PositionAutomaton/PositionAutomaton.h" 
+// struct GraphData {
+//     torch::Tensor x;          // 节点特征
+//     torch::Tensor edge_index; // 边索引 (2, E)
+//     torch::Tensor edge_attr;  // 边特征 (E, d)
+//     torch::Tensor y;          // 图/节点标签
 
-struct GraphData {
-    torch::Tensor x;          // 节点特征
-    torch::Tensor edge_index; // 边索引 (2, E)
-    torch::Tensor edge_attr;  // 边特征 (E, d)
-    torch::Tensor y;          // 图/节点标签
-
-    GraphData(torch::Tensor x_, torch::Tensor edge_index_,
-              torch::Tensor edge_attr_, torch::Tensor y_)
-        : x(x_), edge_index(edge_index_), edge_attr(edge_attr_), y(y_) {}
-};
+//     GraphData(torch::Tensor x_, torch::Tensor edge_index_,
+//               torch::Tensor edge_attr_, torch::Tensor y_)
+//         : x(x_), edge_index(edge_index_), edge_attr(edge_attr_), y(y_) {}
+// };
 
 // Input Regexes
 // Output .pt
@@ -31,7 +29,6 @@ int main(int argc, char* argv[]) {
   std::ifstream infile;
   infile.open(argv[1], std::ios::binary);
   std::string line;
-  bool Ret = true;
   std::vector<std::wstring> Regex_list;
   wchar_t c;
   while (getline(infile, line))
@@ -45,12 +42,11 @@ int main(int argc, char* argv[]) {
     Regex_list.emplace_back(unicodeStr);
     std::vector<solverbin::REnodeClass> ReList;
     std::wcout.sync_with_stdio(true);
+    // we have a list of regexes
+    // Convert to automatas
+    std::wcout << L"Regex: " << unicodeStr << std::endl;
+    auto ren = solverbin::Parser(unicodeStr, false);
+    // auto NFA = solverbin::FollowAtomata(ren.Re);
   }
-    for (auto str : Regex_list){
-      // we have a list of regexes
-      // Convert to automatas
-      std::wcout << L"Regex: " << str << std::endl;
-      auto ren = solverbin::Parser(str, true);
-      auto NFA = solverbin::FollowAtomata(ren.Re);
-    }
-  }
+
+}
