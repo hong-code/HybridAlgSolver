b136e27db406798498e934e96f152261a220085c
2585854600@qq.com <2585854600@qq.com>
2025-12-03 07:45:01 +0000

    12-03

--- Solver/DetectAmbiguity_WithLookAround/ComputeFiniteAmbiguity.cpp
+++ Solver/DetectAmbiguity_WithLookAround/ComputeFiniteAmbiguity.cpp
@@ -9,11 +9,12 @@ namespace solverbin{
 	bool DetectABTNFA_Lookaround::DetectFiniteAmbiguity(){
     auto initState = solverbin::FollowAtomata(this->e1);
     auto dfa = solverbin::DFA(&initState);
-    std::unordered_map<int, std::unordered_map<int, int>> dp;
-    dp[dfa.DState->id][0] = 1;
+    std::unordered_map<int, std::unordered_map<int, std::pair<int, std::string>>> dp;
+    dp[dfa.DState->id][0].first = 1;
     std::queue<std::pair<DFA::DFAState*, int>> stk;
     stk.push(std::pair<DFA::DFAState*, int>(dfa.DState, 0));
-    int MaxLength = 100;
+    int MaxLength = 100000;
+    int ActualMaxLength = 0;
     while (!stk.empty())
     {
       auto CurrentPair = stk.front();
@@ -26,15 +27,19 @@ namespace solverbin{
           std::cout << "Error: NextState NodeSequence size is zero! or NextState is nullable" << std::endl;
           continue;
         }
-        if (dp[CurrentState->id][length] == 0){
+        if (dp[CurrentState->id][length].first == 0){
           std::cout << "Error: dp value is zero!" << std::endl;
         }
-        int NumberOfPaths = dp[CurrentState->id][length];
+        int NumberOfPaths = dp[CurrentState->id][length].first;
         for (auto de : CurrentState->Next[dfa.FA->REClass.ByteMap[c]].second){
           NumberOfPaths += (de - 1);
         }
-        if (NumberOfPaths > dp[NextState->id][length + 1]){
-          dp[NextState->id][length + 1] = NumberOfPaths;
+        if (NumberOfPaths > dp[NextState->id][length + 1].first){
+          dp[NextState->id][length + 1].first = NumberOfPaths;
+          dp[NextState->id][length + 1].second = dp[CurrentState->id][length].second + std::string(1, c);
+          if (length + 1 > ActualMaxLength){
+            ActualMaxLength = length + 1;
+          }
           if (length < MaxLength){
             stk.push(std::pair<DFA::DFAState*, int>(NextState, length + 1));
           } 
@@ -42,15 +47,24 @@ namespace solverbin{
 
       }
     }
+    if (ActualMaxLength < MaxLength){
+      MaxLength = ActualMaxLength;
+    }
     int MaxDegreeOfAmbiguity = 0;
+    int IndexofReturnState;
     for (auto &it1 : dp){
       if(it1.second.find(MaxLength) != it1.second.end()){
-        if (it1.second[MaxLength] > MaxDegreeOfAmbiguity){
-          MaxDegreeOfAmbiguity = it1.second[MaxLength];
+        if (it1.second[MaxLength].first > MaxDegreeOfAmbiguity){
+          IndexofReturnState = it1.first;
+          MaxDegreeOfAmbiguity = it1.second[MaxLength].first;
         }
       }
     }
     std::cout << "MaxDegreeOfAmbiguity: " << MaxDegreeOfAmbiguity << std::endl;
+    std::cout << "witness string with MaxDegreeOfAmbiguity: " << dp[IndexofReturnState][MaxLength].second << std::endl;
+    if (MaxDegreeOfAmbiguity > MaxLength){
+      return true;
+    }
     return false;
   };
 } 
